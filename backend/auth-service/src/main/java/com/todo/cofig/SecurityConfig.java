2b3a53e67d0fc9f1ca4a238579f85bce5b47d559
harshkumar2003 <harshk8876@gmail.com>
2026-02-22 15:01:30 +0530

    feat(auth): add BCrypt password encoder cofig

--- backend/auth-service/src/main/java/com/todo/cofig/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/todo/cofig/SecurityConfig.java
@@ -1,4 +1,19 @@
 package com.todo.cofig;
 
-public class SecurityConfig {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig
+{
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
 }
